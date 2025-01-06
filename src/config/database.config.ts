5e5b69374be824b7786e9c9110393d5862417bef
OwlRepo <romeoangeles010517@gmail.com>
2025-01-06 21:20:12 +0800

    feat: added prod and dev env setup

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,12 +1,30 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('database', () => ({
+const productionConfig = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: process.env.NODE_ENV !== 'production',
+  synchronize: false,
   ssl: {
     rejectUnauthorized: false, // Required for Railway's SSL connection
   },
   autoLoadEntities: true,
-}));
+};
+
+const developmentConfig = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'development',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
+  autoLoadEntities: true,
+};
+
+export default registerAs('database', () => {
+  return process.env.NODE_ENV === 'production'
+    ? productionConfig
+    : developmentConfig;
+});
