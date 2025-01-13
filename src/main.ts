0f7bad7a587bb70e6498a9d86b9dddd7dcfa6a3f
OwlRepo <romeoangeles010517@gmail.com>
2025-01-13 13:12:58 +0800

    refactor: remove unused import in main.ts
    - Removed the unused ValidationPipe import from main.ts to clean up the code and improve readability.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { NestExpressApplication } from '@nestjs/platform-express';
