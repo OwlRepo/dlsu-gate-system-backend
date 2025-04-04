cdccb3c5588dfce909c0dd7294a03925a4f1abf7
OwlRepo <romeoangeles010517@gmail.com>
2025-04-05 02:20:32 +0800

    chore: Update PM2 configuration to specify application port
    - Added PORT environment variable set to 9580 for both development and production environments in ecosystem.config.js, ensuring consistent application behavior across environments.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,9 +10,11 @@ module.exports = {
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'development',
+        PORT: 9580,
       },
       env_production: {
         NODE_ENV: 'production',
+        PORT: 9580,
       },
       exp_backoff_restart_delay: 100,
       merge_logs: true,
